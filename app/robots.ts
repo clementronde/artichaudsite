b3e0334528ef878c49abbfd4f16776b136f3f42c
clementronde <77441856+clementronde@users.noreply.github.com>
2025-11-19 18:37:03 +0100

    Update base URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://artichaud.studio';
+  const baseUrl = 'https://artichaud-studio.com';
 
   return {
     rules: {
